e5c967ec6eed4e1f50b24d425591aff0a93c9e11
daovanduc <duc07092000@gmail.com>
2020-05-08 11:29:54 +0700

    update class CustomerDB

--- model/CustomersDB.php
+++ model/CustomersDB.php
@@ -15,9 +15,9 @@ class CustomersDB
     {
         $sql = "INSERT INTO `customers`(`name`,`email`,`address`) VALUE (?,?,?)";
         $stmt = $this->conn->qrepera($sql);
-        $stmt->bindParam(1,$customer->name);
-        $stmt->bindParam(2,$customer->email);
-        $stmt->bindParam(3,$customer->address);
+        $stmt->bindParam(1, $customer->name);
+        $stmt->bindParam(2, $customer->email);
+        $stmt->bindParam(3, $customer->address);
         return $stmt->execute();
     }
 

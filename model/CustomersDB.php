af3644ee755ab4b5b43c1e835f5bfbbff39a5e3c
daovanduc <duc07092000@gmail.com>
2020-05-08 11:14:46 +0700

    update class CustomerDB

--- model/CustomersDB.php
+++ model/CustomersDB.php
@@ -1,7 +1,24 @@
 <?php
+
 namespace Model;
 
 class CustomersDB
 {
+    public $conn;
+
+    public function __construct($conn)
+    {
+        $this->conn = $conn;
+    }
+
+    public function create($customer)
+    {
+        $sql = "INSERT INTO `customers`(`name`,`email`,`address`) VALUE (?,?,?)";
+        $stmt = $this->conn->qrepera($sql);
+        $stmt->bindParam(1,$customer->name);
+        $stmt->bindParam(2,$customer->email);
+        $stmt->bindParam(3,$customer->address);
+        return $stmt->execute();
+    }
 
 }
